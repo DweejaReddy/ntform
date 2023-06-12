aff2f45de19853db18203c9e5031a843214f8c2b
Dweeja Reddy <dweeja54@gmail.com>
2023-06-12 13:41:03 +0530

    commit 1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Form from "./components/Form";
+import "./App.css";
+import "./css/home.css";
+function App() {
+  return (
+    <div className="bgback">
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
